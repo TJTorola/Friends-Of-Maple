d11e4d8170ec396967d16815e1dff91ddd54c29b
Tyler Torola <tyler@tjt.codes>
2017-03-19 13:02:52 -0700

    Add a nav bar

--- src/view/body.jsx
+++ src/view/body.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const Body = () => <div id="body"></div>;
+const Nav = () => (
+  <div className="nav">
+    <div className="step is-active">Amount</div>
+    <div className="step">Info</div>
+    <div className="step">Payment</div>
+  </div>
+);
+
+const Body = () => (
+  <div id="body">
+    <Nav />
+  </div>
+);
 
 export default Body;
