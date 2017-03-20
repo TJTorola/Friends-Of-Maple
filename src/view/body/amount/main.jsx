bf87768832c6e16fd34dabcc42bc3d95a57321d2
Tyler Torola <tyler@tjt.codes>
2017-03-19 17:52:31 -0700

    Add view logic for amount selection

--- src/view/body/amount/main.jsx
+++ src/view/body/amount/main.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Amount = () => <div>Amount</div>;
+const Amount = () => (
+  <div id="amount">
+    <div className="value-select">
+      <div className="option is-selected">$ 10</div>
+      <div className="option">$ 20</div>
+      <div className="option">$ 50</div>
+      <div className="option">$ 75</div>
+      <div className="option last">$ 100</div>
+    </div>
+  </div>
+);
 
 export default Amount;
