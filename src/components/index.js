bfb9814b0614a5f2a6d7861297997e69bd60cbca
Tyler Torola <tyler@tjt.codes>
2017-03-25 14:30:23 -0700

    Add input to component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as AmountSelector } from './amountSelector/main';
+export { default as Input } from './input';
--- src/view/body/info/main.jsx
+++ src/view/body/info/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AmountSelector } from '~/components/index';
+import { AmountSelector, Input } from '~/components/index';
 import { COPY } from '~/config';
 const copy = COPY.BODY.INFO;
 
@@ -8,6 +8,7 @@ const Info = () => (
   <div id="info">
     <AmountSelector />
     <h2>{ copy.H2 }</h2>
+    <Input placeholder="Placeholder" />
   </div>
 );
 
