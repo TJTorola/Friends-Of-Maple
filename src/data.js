f5fcd661648f9edb92fc6f633558e544582884c4
Tyler Torola <tyler@tjt.codes>
2017-03-25 19:58:04 -0700

    Add some more data

--- src/data.js
+++ src/data.js
@@ -50,3 +50,26 @@ export const STATES = [
   'Wisconsin',
   'Wyoming',
 ];
+
+export const MONTHS = [
+  '01 - January',
+  '02 - February',
+  '03 - March',
+  '04 - April',
+  '05 - May',
+  '06 - June',
+  '07 - July',
+  '08 - August',
+  '09 - September',
+  '10 - October',
+  '11 - November',
+  '12 - December',
+];
+
+export const YEARS = nextNYears(10).map(y => y.toString());
+
+const nextNYears = (n, years = [], year = new Date().getFullYear()) => (
+  (n > 0)
+    ? nextNYears(n - 1, [...years, year], year + 1)
+    : years
+);
