a75cd65ed3b90e0c1a237e7603c2efaa863ac772
Tyler Torola <tyler@tjt.codes>
2017-03-27 09:22:06 -0700

    Add credit card mask

--- src/lib/masks.js
+++ src/lib/masks.js
@@ -4,3 +4,9 @@ const mask = (pattern) => (input) => toPattern(input, pattern);
 
 export const toPhone = mask("(999) 999-9999");
 export const toZip = mask("99999-9999")
+export const toCardNumber = (input) => (
+  (input.startsWith('34') || input.startsWith('37'))
+    ? toPattern(input, '9999 999999 99999')
+    : toPattern(input, '9999 9999 9999 9999')
+);
+export const toExperation = mask("99/99");
