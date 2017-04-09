1ebb8b9c3eae7d6146b8e26d55426a6e183fd2b6
Tyler Torola <tyler@tjt.codes>
2017-04-08 23:03:37 -0700

    Write out basic payment function structure

--- src/middleware/payment.js
+++ src/middleware/payment.js
@@ -1,13 +1,47 @@
 import {
+  setPledgeProcessing,
+  validateInformation,
+  validatePayment,
+} from '~/actions/index';
+import {
   POST_PLEDGE,
 } from '~/actions/types';
 
-const paymentMiddleware = store => next => action => {
-  switch (action.type) {
-    case POST_PLEDGE:
+const validate = (getState, dispatch) = {
+  dispatch(validateInformation());
+  dispatch(validatePayment());
 
-      break;
-    default:
+  const { information, payment } = getState();
+  const hasInformationErrors = Object.keys(information.errors).length > 0;
+  const hasPaymentErrors = Object.keys(payment.errors).length > 0;
 
+  if (hasInformationErrors || hasPaymentErrors) {
+    dispatch(setPledgeProcessing(false));
+    return false;
   }
+  return true;
+}
+
+async function postPledge({ getState, dispatch }) {
+  dispatch(setPledgeProcessing(true));
+
+  if (!validate(getState, dispatch)) {
+    return;
+  };
+
+  try {
+    await getStripeToken(getState, dispatch);
+    await postPlanSubscription(getState, dispatch);
+  } catch (e) {
+    dispatch(setPledgeProcessing(false));
+    dispatch(setPledgeError(e));
+    return;
+  }
+
+  dispatch(setPledgeProcessing(false));
+  dispatch(setPledgeSuccess());
+}
+
+const paymentMiddleware = {
+  [POST_PLEDGE]: (_, store) => postPledge(store);
 }
