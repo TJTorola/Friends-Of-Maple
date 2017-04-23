4691044e967957266e87ee8a3e7ce36a3072dede
Tyler Torola <tyler@tjt.codes>
2017-04-22 19:07:39 -0700

    Redesign processing page

--- src/view/body/processing.jsx
+++ src/view/body/processing.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import Gear from '~/components/gear';
+import Tree from '~/components/tree';
 
 const Processing = () => (
-  <div>
-    <Gear />
+  <div className="processing">
+    <Tree />
+    <div className="content">
+      <Gear />
+      <h2>Processing...</h2>
+    </div>
   </div>
 );
 
