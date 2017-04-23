6b06f36936d896eaf103927113e65a15f37bc1c3
Tyler Torola <tyler@tjt.codes>
2017-04-22 19:42:11 -0700

    Remove default store

--- src/app.jsx
+++ src/app.jsx
@@ -11,11 +11,9 @@ import Reducers from './reducers/index';
 import Middleware from './middleware/index';
 import * as Actions from './actions/index';
 
-const defaultStore = JSON.parse('{"nav":"PAYMENT","planId":"fom-50","information":{"firstName":"t","lastName":"t","email":"t","phone":"","address":"","city":"","state":"","zip":"","errors":{}},"payment":{"cardNumber":"4242 4242 4242 4242","experation":"09/18","name":"Tyler Torola","csv":"232","zip":"94708","errors":{}},"pledge":{"processing":false,"paymentToken":""}}');
-
 const store = createStore(
   Reducers,
-  defaultStore,
+  {},
   Middleware
 );
 global.s = store;
